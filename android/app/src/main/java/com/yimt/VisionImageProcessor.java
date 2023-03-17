eb45a3d6efcb23e84df983ce64a4c6f7a64d4e69
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-03-17 12:05:12 +0800

    Reformat VisionImageProcessor.java.

--- android/app/src/main/java/com/yimt/VisionImageProcessor.java
+++ android/app/src/main/java/com/yimt/VisionImageProcessor.java
@@ -2,12 +2,18 @@ package com.yimt;
 
 import android.graphics.Bitmap;
 
-/** An interface to process the images with different vision detectors and custom image models. */
+/**
+ * An interface to process the images with different vision detectors and custom image models.
+ */
 public interface VisionImageProcessor {
 
-  /** Processes a bitmap image. */
-  void processBitmap(Bitmap bitmap);
+    /**
+     * Processes a bitmap image.
+     */
+    void processBitmap(Bitmap bitmap);
 
-  /** Stops the underlying machine learning model and release resources. */
-  void stop();
+    /**
+     * Stops the underlying machine learning model and release resources.
+     */
+    void stop();
 }
