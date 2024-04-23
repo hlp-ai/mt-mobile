f02dda3259d2e8c5f0c173bc769fbfdf23e16688
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-04-23 20:41:29 +0800

    使用语言下列列表，增加删除和拷贝事件处理程序。

--- android/app/src/main/java/com/yimt/TextActivity.java
+++ android/app/src/main/java/com/yimt/TextActivity.java
@@ -1,15 +1,69 @@
 package com.yimt;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.yimt.databinding.ActivityTextBinding;
 
 
 public class TextActivity extends AppCompatActivity {
+
+    private ActivityTextBinding binding;
+    private String[] languages = new String[] {"自动检测", "中文", "英文"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_text);
+        binding = ActivityTextBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayAdapter<String> srcLangAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, languages);
+        srcLangAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerSrcLang.setAdapter(srcLangAdapter);
+
+        ArrayAdapter<String> tgtLangAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, languages);
+        tgtLangAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerTgtLang.setAdapter(tgtLangAdapter);
+        binding.spinnerTgtLang.setSelection(1);
+
+        // 删除按钮
+        binding.RemoveSourceText.setOnClickListener(view -> {
+            if (!binding.textSource.getText().toString().equals("")) {
+                binding.textSource.setText("");
+                binding.textTarget.setText("");
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                View view1 = getCurrentFocus();
+                if (view1 != null) {
+                    imm.hideSoftInputFromWindow(view1.getWindowToken(), 0);
+                }
+            }
+
+        });
+
+        // 拷贝按钮
+        binding.CopyTranslation.setOnClickListener(view -> {
+            if (!binding.textTarget.getText().toString().equals("")) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("translation", binding.textTarget.getText());
+                clipboard.setPrimaryClip(clip);
+                Snackbar.make(
+                        binding.CopyTranslation,
+                        getString(R.string.copiedClipboard),
+                        Snackbar.LENGTH_LONG
+                ).show();
+            }
+        });
     }
 }
