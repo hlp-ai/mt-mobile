5f4ba21802794106982d4c12bd533d6106701b12
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-04-23 08:55:59 +0800

    添加首页和文本窗口。

--- android/app/src/main/java/com/yimt/StartActivity.java
+++ android/app/src/main/java/com/yimt/StartActivity.java
@@ -2,7 +2,10 @@ package com.yimt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        TextView txtInput = findViewById(R.id.text_input);
+        txtInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, TextActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- android/app/src/main/java/com/yimt/TextActivity.java
+++ android/app/src/main/java/com/yimt/TextActivity.java
@@ -1,14 +1,15 @@
 package com.yimt;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class TextActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_text);
     }
 }
