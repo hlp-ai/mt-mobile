07b145808665c45fa8dc94747d585adf0491440c
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-04-25 15:56:15 +0800

    添加图库处理代码。

--- android/app/src/main/java/com/yimt/TextActivity.java
+++ android/app/src/main/java/com/yimt/TextActivity.java
@@ -6,12 +6,16 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.TypedValue;
 import android.view.MotionEvent;
@@ -26,6 +30,7 @@ import androidx.core.app.ActivityCompat;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.yimt.databinding.ActivityTextBinding;
+import com.yimt.ocr.BitmapUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -41,6 +46,9 @@ public class TextActivity extends AppCompatActivity {
     private boolean isRecording = false;
     private MediaRecorder mediaRecorder = null;
     private String audioFile = null;
+
+    private static final int REQUEST_CHOOSE_IMAGE = 101;
+    private static final int REQUEST_CROP_IMAGE = 102;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -103,6 +111,14 @@ public class TextActivity extends AppCompatActivity {
 //            }
 //        });
 
+        // 图库按钮
+        binding.Gallery.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_CHOOSE_IMAGE);
+        });
+
         // 删除按钮
         binding.RemoveSourceText.setOnClickListener(view -> {
             if (!binding.textSource.getText().toString().equals("")) {
@@ -135,6 +151,41 @@ public class TextActivity extends AppCompatActivity {
         binding.ReadTranslation.setOnClickListener(v -> {
             playAudio();
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Uri imageUri;
+        if (requestCode == REQUEST_CHOOSE_IMAGE && resultCode == RESULT_OK) {
+            Bitmap imageBitmap = null;
+            imageUri = data.getData();
+            Toast.makeText(TextActivity.this, imageUri.toString(), Toast.LENGTH_LONG).show();
+            try {
+                imageBitmap = BitmapUtils.getBitmapFromContentUri(getContentResolver(), imageUri);
+                if(imageBitmap==null)
+                    Toast.makeText(TextActivity.this, "null image", Toast.LENGTH_LONG).show();
+                else
+                    crop(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == REQUEST_CROP_IMAGE && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            Toast.makeText(TextActivity.this, imageUri.toString(), Toast.LENGTH_LONG).show();
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void crop(Bitmap bitmap) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        Uri uri = Uri.parse(MediaStore.Images.Media.insertImage(TextActivity.this.getContentResolver(), bitmap, "1", "1"));
+        intent.setDataAndType(uri, "image/*");//设置要缩放的图片Uri和类型
+        intent.putExtra("crop", "true");
+        intent.putExtra("scale", true);//缩放
+        intent.putExtra("return-data", false);//当为true的时候就返回缩略图，false就不返回，需要通过Uri
+        intent.putExtra("noFaceDetection", false);//前置摄像头
+        startActivityForResult(intent, REQUEST_CROP_IMAGE);//打开剪裁Activity
     }
 
     //开始录音
