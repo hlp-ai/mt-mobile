fefee98f5beb3e34da00e0d34055b391c5648618
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-04-28 16:25:37 +0800

    添加同意服务请求方法。

--- android/app/src/main/java/com/yimt/Utils.java
+++ android/app/src/main/java/com/yimt/Utils.java
@@ -4,10 +4,19 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.util.Base64;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class Utils {
 
@@ -42,6 +51,39 @@ public class Utils {
         mediaPlayer.setOnCompletionListener(mp -> {
             mediaPlayer.release();
         });
+    }
+
+    public static JSONObject request(String urlString, String data) throws IOException, JSONException {
+        int CONN_TIMEOUT = 15000;
+        int READ_TIMEOUT = 15000;
+
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(CONN_TIMEOUT);
+        conn.setReadTimeout(READ_TIMEOUT);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+
+        // 发送请求数据
+        OutputStream os = conn.getOutputStream();
+        os.write(data.getBytes(StandardCharsets.UTF_8));
+        os.close();
+
+        // 获取并处理响应数据
+        InputStream is = conn.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line;
+        StringBuilder response = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+        reader.close();
+        is.close();
+        conn.disconnect();
+
+        return new JSONObject(response.toString());
     }
 
 //    // 开始录制声音
