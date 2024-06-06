6084acbafc67e94ff3c4aca5d03b11400dbd97ef
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-06-06 11:02:54 +0800

    添加广告获取代码。

--- android/app/src/main/java/com/yimt/FlashActivity.java
+++ android/app/src/main/java/com/yimt/FlashActivity.java
@@ -3,9 +3,15 @@ package com.yimt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FlashActivity extends AppCompatActivity {
 
@@ -13,6 +19,9 @@ public class FlashActivity extends AppCompatActivity {
     private TextView timeView;
 
     private Handler handler = new Handler();
+
+    private SharedPreferences settings;
+    private final static String DEFAULT_SERVER = "http://192.168.1.104:5555";
 
     private static final int TIMEOUT = 4;
     private int left = TIMEOUT;
@@ -40,8 +49,25 @@ public class FlashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash);
 
+        settings = getSharedPreferences("com.yimt", 0);
+        String server = settings.getString("server", DEFAULT_SERVER);
+        String apiKey = settings.getString("apiKey", "");
+
+        String ad = "广告待展示...";
+
+        JSONObject adMsg = new JSONObject();
+        try {
+            adMsg = requestAD(server);
+
+            String adID = adMsg.getString("ad_id");
+            ad = adMsg.getString("content");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(FlashActivity.this, "连接不上服务器", Toast.LENGTH_SHORT).show();
+        }
+
         adView = findViewById(R.id.AD);
-        adView.setText("广告内容展示区...");
+        adView.setText(ad);
 
         timeView = findViewById(R.id.TIME);
         timeView.setText(left + "秒");
@@ -56,5 +82,50 @@ public class FlashActivity extends AppCompatActivity {
 
         handler.removeCallbacks(startRunnable);
         handler.removeCallbacks(timeRunnable);
+    }
+
+//    private void getAD() {
+//        String server = settings.getString("server", DEFAULT_SERVER);
+//        String apiKey = settings.getString("apiKey", "");
+//
+//        Thread thread = new Thread(() -> {
+//            String error = "";
+//            JSONObject adMsg = new JSONObject();
+//            try {
+//                adMsg = requestAD(server);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                error = e.toString();
+//            }
+//
+//            Bundle bundle = new Bundle();
+//            bundle.putString("error", error);
+//            if(error.isEmpty()){
+//                try {
+//                    bundle.putString("ad_id", adMsg.getString("ad_id"));
+//                    bundle.putString("type", adMsg.getString("type"));
+//                    bundle.putString("content", adMsg.getString("content"));
+//                    bundle.putString("url", adMsg.getString("url"));
+//                } catch (JSONException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//            Message msg = new Message();
+//            msg.setData(bundle);
+//            msg.what = GET_AD;
+//            mhandler.sendMessage(msg);
+//        });
+//        thread.start();
+//    }
+
+    private JSONObject requestAD(String server) throws Exception {
+        String url = server + "/request_ad";
+
+        JSONObject json = new JSONObject();
+        json.put("platform", "app");
+
+        JSONObject responseJson = Utils.requestService(url, json.toString());
+
+        return responseJson;
     }
 }
