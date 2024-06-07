32e91fbba5d52857e1a84373b09cfd1ed05a0705
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-06-07 10:54:14 +0800

    修改广告请求问题；广告可跳过；

--- android/app/src/main/java/com/yimt/FlashActivity.java
+++ android/app/src/main/java/com/yimt/FlashActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,12 +14,15 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class FlashActivity extends AppCompatActivity {
 
     private TextView adView;
     private TextView timeView;
 
     private Handler handler = new Handler();
+    private Handler mhandler;
+    private static final int GET_AD = 401;
 
     private SharedPreferences settings;
     private final static String DEFAULT_SERVER = "http://192.168.1.104:5555";
@@ -39,7 +43,7 @@ public class FlashActivity extends AppCompatActivity {
         @Override
         public void run() {
             left = left - 1;
-            timeView.setText(left + "秒");
+            timeView.setText("跳过 " + left);
             handler.postDelayed(this, 1000);
         }
     };
@@ -53,24 +57,37 @@ public class FlashActivity extends AppCompatActivity {
         String server = settings.getString("server", DEFAULT_SERVER);
         String apiKey = settings.getString("apiKey", "");
 
-        String ad = "广告待展示...";
-
-        JSONObject adMsg = new JSONObject();
-        try {
-            adMsg = requestAD(server);
-
-            String adID = adMsg.getString("ad_id");
-            ad = adMsg.getString("content");
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(FlashActivity.this, "连接不上服务器", Toast.LENGTH_SHORT).show();
-        }
-
         adView = findViewById(R.id.AD);
-        adView.setText(ad);
+
+        mhandler = new Handler(Looper.getMainLooper()) {
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                if (msg.what == GET_AD) {
+                    Bundle lc = msg.getData();
+                    String ad = lc.getString("content");
+                    String serverError = lc.getString("error");
+                    if (serverError.length() > 0)
+                        Toast.makeText(FlashActivity.this, serverError, Toast.LENGTH_LONG).show();
+                    else
+                        adView.setText(ad);
+                }
+            }
+        };
+
+        getAD();
 
         timeView = findViewById(R.id.TIME);
-        timeView.setText(left + "秒");
+        timeView.setText("跳过 " + left);
+        timeView.setOnClickListener(view -> {
+            Intent mainIntent = new Intent(FlashActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+
+            handler.removeCallbacks(startRunnable);
+            handler.removeCallbacks(timeRunnable);
+
+            finish();
+        });
 
         handler.postDelayed(timeRunnable, 1000);
         handler.postDelayed(startRunnable, TIMEOUT * 1000);
@@ -84,39 +101,39 @@ public class FlashActivity extends AppCompatActivity {
         handler.removeCallbacks(timeRunnable);
     }
 
-//    private void getAD() {
-//        String server = settings.getString("server", DEFAULT_SERVER);
-//        String apiKey = settings.getString("apiKey", "");
-//
-//        Thread thread = new Thread(() -> {
-//            String error = "";
-//            JSONObject adMsg = new JSONObject();
-//            try {
-//                adMsg = requestAD(server);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                error = e.toString();
-//            }
-//
-//            Bundle bundle = new Bundle();
-//            bundle.putString("error", error);
-//            if(error.isEmpty()){
-//                try {
-//                    bundle.putString("ad_id", adMsg.getString("ad_id"));
-//                    bundle.putString("type", adMsg.getString("type"));
-//                    bundle.putString("content", adMsg.getString("content"));
-//                    bundle.putString("url", adMsg.getString("url"));
-//                } catch (JSONException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//            Message msg = new Message();
-//            msg.setData(bundle);
-//            msg.what = GET_AD;
-//            mhandler.sendMessage(msg);
-//        });
-//        thread.start();
-//    }
+    private void getAD() {
+        String server = settings.getString("server", DEFAULT_SERVER);
+        String apiKey = settings.getString("apiKey", "");
+
+        Thread thread = new Thread(() -> {
+            String error = "";
+            JSONObject adMsg = new JSONObject();
+            try {
+                adMsg = requestAD(server);
+            } catch (Exception e) {
+                e.printStackTrace();
+                error = e.toString();
+            }
+
+            Bundle bundle = new Bundle();
+            bundle.putString("error", error);
+            if(error.isEmpty()){
+                try {
+                    bundle.putString("ad_id", adMsg.getString("ad_id"));
+                    bundle.putString("type", adMsg.getString("type"));
+                    bundle.putString("content", adMsg.getString("content"));
+                    bundle.putString("url", adMsg.getString("url"));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            Message msg = new Message();
+            msg.setData(bundle);
+            msg.what = GET_AD;
+            mhandler.sendMessage(msg);
+        });
+        thread.start();
+    }
 
     private JSONObject requestAD(String server) throws Exception {
         String url = server + "/request_ad";
