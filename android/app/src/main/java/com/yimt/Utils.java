a29c41fc247870bfd787fa997f3291ef34db40a4
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-10-06 20:41:50 +0800

    删除无用代码。

--- android/app/src/main/java/com/yimt/Utils.java
+++ android/app/src/main/java/com/yimt/Utils.java
@@ -1,8 +1,6 @@
 package com.yimt;
 
 import android.graphics.Bitmap;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
 import android.util.Base64;
 import android.util.Log;
 
@@ -11,11 +9,8 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -24,39 +19,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class Utils {
-
-    // 播放音频数据
-    public static void playAudio(String audio, String type) throws IOException {
-        byte[] audioData = Base64.decode(audio, Base64.DEFAULT);
-
-        File tempAudioFile = File.createTempFile("temp_audio", "." + type);
-
-        OutputStream os = new FileOutputStream(tempAudioFile);
-        os.write(audioData);
-        os.close();
-
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        mediaPlayer.setDataSource(tempAudioFile.getAbsolutePath());
-        mediaPlayer.prepare();
-        mediaPlayer.start();
-
-        mediaPlayer.setOnCompletionListener(mp -> {
-            mediaPlayer.release();
-            tempAudioFile.delete();
-        });
-    }
-
-    // 播放音频文件
-    public static void playAudio(String audioFile) throws IOException {
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        mediaPlayer.setDataSource(audioFile);
-        mediaPlayer.prepare();
-        mediaPlayer.start();
-
-        mediaPlayer.setOnCompletionListener(mp -> {
-            mediaPlayer.release();
-        });
-    }
 
     // 服务请求
     public static String requestService(String urlString, String data, String method) throws IOException {
@@ -159,13 +121,6 @@ public class Utils {
 
             return byteArrayOutputStream.toByteArray();
         }
-    }
-
-    public static String encodeImageToBase64(Bitmap imageBitmap) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-        byte[] imageBytes = byteArrayOutputStream.toByteArray();
-        return Base64.encodeToString(imageBytes, Base64.DEFAULT);
     }
 
 }
